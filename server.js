24008fe3d280938c110726e8a294ae21c90f1174
Hackintosh-02 <ashutoshsharma2625@gmail.com>
2024-08-25 11:49:27 +0530

    change port setting

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import schoolRoutes from "./src/routes/schoolRoutes.js";
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api',schoolRoutes);
